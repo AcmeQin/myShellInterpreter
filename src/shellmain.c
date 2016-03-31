7743bf6f5fc557c3a8bd6604963c23260508876e
qinacme <qinhehahaha@hotmail.com>
2016-03-31 08:02:27 +0800

    [qinacme]: Finish read line. Finish exit.

--- src/shellmain.c
+++ src/shellmain.c
@@ -10,7 +10,8 @@
 
 int main(int argc, char *argv[]) {
   char pipeName[PIPE_NAME_LEN];
-  char *buf=NULL;
+  char buf[MAX];
+  char _exit[]="exit";
   int bytesread;
   size_t len = 0;
   ssize_t read;
@@ -20,9 +21,13 @@ int main(int argc, char *argv[]) {
   printf("pipeName: %s\n", pipeName);
 
   FILE *fd = fopen( pipeName, "r" );
-
-  while ((read = getline(&buf, &len, fd)) != -1) {
-    printf("%s\n", buf);
+  while (1) {
+    while (fgets(buf,MAX,fd)!=NULL) {
+      buf[strcspn(buf,"\n")]=0;
+      printf("%s\n", buf);
+    }
+    if (strcmp(buf,"exit")==0)
+      break;
   }
 
   fclose(fd);
