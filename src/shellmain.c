67ad3d9b7777a687e6c90239784815c17fedc404
qinacme <qinhehahaha@hotmail.com>
2016-04-05 18:37:13 +0800

    [qinacme]: Finish cd. Finish ls.

--- src/shellmain.c
+++ src/shellmain.c
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <regex.h>
 #include <unistd.h>
+#include <dirent.h>
 
 #define MAX 1024
 #define PIPE_NAME_LEN 100
@@ -13,6 +14,8 @@
 regex_t regex;
 int reti;
 char dir[MAX];
+DIR *d;
+struct dirent *dirCont;
 
 void execute(char *cmd);
 
@@ -23,7 +26,7 @@ int main(int argc, char *argv[]) {
   size_t len = 0;
   ssize_t read;
   // Compile regular expression
-  reti = regcomp(&regex,"^cd$",0);
+  reti = regcomp(&regex,"^cd[ ].*$",0);
   if (reti) {
     fprintf(stderr, "Could not compile regex\n");
     exit(1);
@@ -39,7 +42,7 @@ int main(int argc, char *argv[]) {
   while (1) {
     while (fgets(buf,MAX,fd)!=NULL) {
       buf[strcspn(buf,"\n")]=0;
-      printf("%s\n", buf);
+      printf(">>> %s\n", buf);
       execute(buf);
     }
     if (strcmp(buf,"exit")==0)
@@ -54,6 +57,7 @@ int main(int argc, char *argv[]) {
 // This method gets command input, uses regular expression to category and executes correspondingly
 
 void execute(char *cmd){
+  // pwd command
   if (strcmp(cmd,"pwd")==0){
     if (getcwd(dir, sizeof(dir)) != NULL)
       fprintf(stdout, "%s\n", dir);
@@ -61,10 +65,31 @@ void execute(char *cmd){
       perror("getcwd() error");
     return;
   }
+
+  // ls command
+  if (strcmp(cmd,"ls")==0){
+    if (getcwd(dir, sizeof(dir)) != NULL){
+      d=opendir(dir);
+      if (d){
+        while ((dirCont = readdir(d)) != NULL){
+          printf("%s\n", dirCont->d_name);
+        }
+        closedir(d);
+      }
+    }
+    else
+      perror("getcwd() error");
+    return;
+  }
+
+  // cd command
     /* Execute regular expression */
   reti = regexec(&regex, cmd, 0, NULL, 0);
   if (!reti) {
       puts("Match");
+      size_t len = strlen(cmd);
+      memmove(cmd, cmd+3, len - 3 + 1);
+      reti = chdir(cmd);
   }
   else if (reti == REG_NOMATCH) {
       puts("No match");
